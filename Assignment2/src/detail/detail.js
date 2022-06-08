c31353bc9e2e073284f4247d57e192608525a74a
Vũ Anh Tú <>
2022-06-08 16:07:20 +0700

    update

--- Assignment2/src/detail/detail.js
+++ Assignment2/src/detail/detail.js
@@ -1,7 +1,33 @@
 import data from '../../db.json' assert {type: 'json'}
 
-const params = new URLSearchParams(window.location.search);
-const id = params.get('id')
+const urlParams = new URLSearchParams(location.search);
 
-const book = data.filter(book => book.id == id)
-console.log(book)
+const id = urlParams.get('id')
+
+let book = data.filter(function(book) {
+    return book.id == id
+})
+
+book = book[0]
+
+const contentElement = document.querySelector('#content')
+
+const _content = /*html*/`
+    <div class="container mx-auto flex">
+        <div class="w-1/4 flex-none">
+            <img src="${book.images[0].base_url}">
+            <div class="grid grid-cols-5 gap-3">
+                ${book.images.map(function(image) {
+                    return `<img src="${image.base_url}" alt="">`
+                }).join('')}
+            </div>
+        </div>
+        <div class="grow">
+            <p>Tác giả</p>
+            <h3>Càng Kỷ Luật, Càng Tự Do</h3>
+            <p>Sao</p>
+            <p>Price</p>
+        </div>
+    </div>
+`
+contentElement.innerHTML = _content
