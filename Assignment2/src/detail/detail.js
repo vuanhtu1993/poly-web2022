879db6843698c02eb60f4d3e66167ffb356bc7af
Vũ Anh Tú <>
2022-06-11 12:52:30 +0700

    same product and specification

--- Assignment2/src/detail/detail.js
+++ Assignment2/src/detail/detail.js
@@ -1,33 +1,65 @@
-import data from '../../db.json' assert {type: 'json'}
+import data from "../../db.json" assert { type: "json" };
 
-const urlParams = new URLSearchParams(location.search);
-
-const id = urlParams.get('id')
+const params = new URLSearchParams(window.location.search)
 
 let book = data.filter(function(book) {
-    return book.id == id
+    return book.id == params.get('id')
 })
 
 book = book[0]
+console.log(book)
+const category = book.categories.name;
 
 const contentElement = document.querySelector('#content')
 
 const _content = /*html*/`
-    <div class="container mx-auto flex">
-        <div class="w-1/4 flex-none">
-            <img src="${book.images[0].base_url}">
+    <div class="container mx-auto flex space-x-3">
+        <div class="w-1/3 flex-none">
+            <img id="detail-image" src="${book.images[0].base_url}">
             <div class="grid grid-cols-5 gap-3">
+                
                 ${book.images.map(function(image) {
-                    return `<img src="${image.base_url}" alt="">`
+                    const _image = document.createElement('img')
+                    _image.src = image.base_url
+                    return `${_image}`
                 }).join('')}
             </div>
         </div>
         <div class="grow">
-            <p>Tác giả</p>
-            <h3>Càng Kỷ Luật, Càng Tự Do</h3>
-            <p>Sao</p>
-            <p>Price</p>
+            <p>${book.authors ? book.authors[0].name : "Không xác định tác giả"}</p>
+            <p>${book.original_price}</p>
+            <div class="flex space-x-2 items-center">
+                <p>⭐⭐⭐⭐</p>
+                <p>DDanh gias</p>
+                <p>Da ban</p>
+            </div>
+        </div>
+    </div>
+    <div>
+        Thông tin chi tiết sp
+        ${book.specifications && book.specifications[0].attributes.map(function(info) {
+            return /*html*/`
+                <div><span>${info.name}: ${info.value}</span></div>
+            `
+        })}
+    </div>
+    <div>
+        Sản phẩm tương tự
+        <div class="grid grid-cols-8 gap-3">
+        ${data.filter(function(book) {
+            return book.categories.name == category
+        }).map(function(book) {
+            return /*html*/`
+                <div>
+                    <img src="${book.images[0].base_url}">
+                    <p>${book.name}</p>
+                </div>
+            `
+        }).join('')}
         </div>
     </div>
 `
+
 contentElement.innerHTML = _content
+
+const detailImageElement = document.querySelector('#detail-image')
